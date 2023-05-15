aecb1082770961fdbec2f368d310ccb0987d5777
Vika <viktoria.wout@gmail.com>
2023-05-15 19:16:39 +0200

    import postgre added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'balarama.db.elephantsql.com',
+      port: 3306,
+      username: 'vtdrniub',
+      password: '0EkRfOaCjFbcZfF4EH1gKoF-cPG7FWvk',
+      database: 'vtdrniub',
+      entities: [],
+      synchronize: true,
+    }), UsersModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
