863401b8ce28d172ae9a3077c3822dbbc2bc5077
Vika <viktoria.wout@gmail.com>
2023-05-15 14:56:43 +0200

    swagger installed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
-  app.enableCors({credentials: true, origin: true})
+  app.enableCors({ credentials: true, origin: true });
+  const config = new DocumentBuilder()
+    .setTitle('Cloud Storage')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(7777);
 }
 bootstrap();
